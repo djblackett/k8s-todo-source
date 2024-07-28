6243ac9646ca61c2b1bf81eec6070891d6c7a345
David Andrea <davejandrea@gmail.com>
2024-07-28 20:53:58 -0300

    update broadcaster and github workflow

---
+++ broadcaster/subscriber.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/gtuk/discordwebhook"
+	"github.com/nats-io/nats.go"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	logger := log.New(os.Stdout, "", log.LstdFlags)
+	var environment = os.Getenv("ENVIRONMENT")
+
+	// Connect to nats server
+	var natsUrl = os.Getenv("NATS_URL")
+	nc, _ := nats.Connect(natsUrl)
+	var content string
+	var username = "djblackett's bot"
+
+	var url = os.Getenv("WEBHOOK_URL")
+	r := gin.Default()
+
+	message := discordwebhook.Message{
+		Username: &username,
+		Content:  &content,
+	}
+
+	// Simple Async Subscriber
+	nc.QueueSubscribe("broadcaster", "broadcast-workers", func(m *nats.Msg) {
+		fmt.Printf("Received a message: %s\n", string(m.Data))
+		content = string(m.Data)
+
+		if environment == "staging" {
+			logger.Println("Message: ", content, time.Now().String())
+		} else if environment == "production" {
+			err := discordwebhook.SendMessage(url, message)
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			log.Fatalln("ENVIRONMENT not set")
+		}
+	})
+
+	// required for GKE
+	r.GET("/", func(c *gin.Context) {
+		c.Status(http.StatusOK)
+	})
+
+	// For liveness and readiness probes
+	r.GET("/healthz", func(c *gin.Context) {
+		fmt.Println("Checking health")
+
+		if nc != nil && nc.Status() == nats.CONNECTED {
+			c.Status(http.StatusOK)
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "nats not connected"})
+		}
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":8500"
+	}
+
+	r.Run(port)
+}
